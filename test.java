e2404ddfaebfdcd2d96dbf3e855f290b15d80fc7
anthonymarze <anthonymarze@gmail.com>
2019-12-10 17:51:47 -0500

    initial java notes

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+        final int myNum = 15;
+        int[] arr = {1, 2, 3};
+        double bb = 3.0;
+        System.out.println("Hello World");
+        System.out.println(myNum<<1);
+        System.out.println(4 % bb);
+        System.out.println(arr[1]);
+    }
+}
