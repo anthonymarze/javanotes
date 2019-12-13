dbf44c048c57a8c679e0bf55d2f82e7fd239e881
anthonymarze <anthonymarze@gmail.com>
2019-12-13 17:16:59 -0500

    simple input prompt test

--- In.java
+++ In.java
@@ -3,13 +3,28 @@ import java.util.*;
 public class In {
     public static void main(String[] args) {
         ArrayList<String> words = new ArrayList<String>();
+        ArrayList<String> answers = new ArrayList<String>();
 
         words.add("one");
         words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+
 
         Scanner input = new Scanner(System.in);
-        String text = input.nextLine();
-        System.out.println(words);
+
+        for (String word : words) {
+            System.out.println("type " + word + ": ");
+            String text = input.nextLine();
+            answers.add(text);
+        }
+
         input.close();
+        System.out.println("the answers are: ");
+
+        for (String answer : answers) {
+            System.out.println(answer);
+        }
     }
 }
